86629d5247c222b8ac27fd87f8bc95d82e5c72da
Kristalev Vsevolod <vsevolod.kristalev@gmail.com>
2021-06-02 22:51:05 +0600

    add board draw

--- RushHour.py
+++ RushHour.py
@@ -2,48 +2,69 @@ import tkinter as tk
 from PIL import ImageTk, Image
 import random
 
-# class for tiles info
-class Tile:
 
-    def __init__(self, size, direction, tileCoordX, tileCoordY, keyTile):
 
-        self.size = size # 2, 3
+# draw game board
+# 0 - empty,
+# 1 - wall,
+# 8 - key,
+# 9 - lock
+# other numbers - moveble blocks
+def draw_board():
+    for row in range(len(board)):
+        for col in range(len(board[row])):
+            if board[row][col] == 0:
+                canvas.create_rectangle(col * squareSize, row * squareSize, # white cube
+                                                col * squareSize + squareSize,
+                                                row * squareSize + squareSize,
+                                                fill="#55342b",
+                                                outline="#8b5546")
 
-        self.direction = direction # horizontal, vertical
-
-        self.tileCoordX = tileCoordX # X tile coordinate
-        self.tileCoordY = tileCoordY # Y tile coordinate
-
-        self.tileIndexX = None # X index to choose tile
-        self.tileIndexY = None # Y index to choose tile
-
-        self.tileStatus = "UNSELECT"
-
-        self.keyTile = keyTile
+            elif board[row][col] == 1:
+                canvas.create_rectangle(col * squareSize, row * squareSize, # area blocks
+                                            col * squareSize + squareSize,
+                                            row * squareSize + squareSize,
+                                            fill="#000000")
+            
+            elif board[row][col] == 8:
+                key = canvas.create_rectangle(col * squareSize, row * squareSize,
+                                            col * squareSize + squareSize,
+                                            row * squareSize + squareSize,
+                                            fill="#D42421",
+                                            outline="#8b5546")
+            elif board[row][col] == 9:
+                canvas.create_rectangle(col * squareSize, row * squareSize, # area blocks
+                                            col * squareSize + squareSize,
+                                            row * squareSize + squareSize,
+                                            fill="#ffffff",
+                                            outline="#8b5546")
+            else:
+                canvas.create_rectangle(col * squareSize, row * squareSize, # area blocks
+                                            col * squareSize + squareSize,
+                                            row * squareSize + squareSize,
+                                            fill="#FFFF99",
+                                            outline="#0F0326")
     
-    def set_coords(self, tileCoordX, tileCoordY):
-        self.tileCoordX = tileCoordX
-        self.tileCoordY = tileCoordY
-    
-    def get_index(self):
-        self.tileIndexX = self.tileCoordX // squareSize
-        self.tileIndexY = self.tileCoordY // squareSize
-    
-    def set_direction(self, direction):
-        self.direction = direction
-    
-    def set_size(self, size):
-        self.size = size
 
-    def set_status(self, TileStatus):
-        self.tileStatus = tileStatus
+# Game settings
+root = tk.Tk() # main window
+root.title("Slide Blocks") # window name
 
-    def description(self):
-        print("size =", self.size, " direction =", self.direction, " TileCoordX =", 
-            self.TileCoordX, " TileCoordY =", self.TileCoordY, " TileStatus =", self.TileStatus)
 
-class Field:
+# create board array
+board = [[1, 1, 9, 1, 1, 1],
+         [1, 0, 2, 2, 3, 1],
+         [1, 0, 8, 0, 3, 1],
+         [1, 0, 8, 0, 0, 1],
+         [1, 0, 0, 0, 0, 1],
+         [1, 1, 1, 1, 1, 1]]
 
-    def __init__(self, boardSize):
-        self.boardSize = boardSize
-        self.emptySq
+squareSize = 100
+
+canvas = tk.Canvas(root, width = len(board[0]) * squareSize, height = len(board) * squareSize, bg = "#808080") # create canvas
+canvas.focus_set() # focus on canvas 
+canvas.pack() # display canvas
+
+draw_board()
+
+root.mainloop()
